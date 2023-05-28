cbf723938a422a2481767d127b6af6c1a4b1b136
Yuna1002 <e2431666@gmail.com>
2023-05-28 13:47:26 +0800

    render

--- backProducts.js
+++ backProducts.js
@@ -1,18 +1,32 @@
 const url = "https://vue3-course-api.hexschool.io/v2";
+const path = "yuna1002";
 const app = {
   data() {
-    return {};
+    return {
+      products: [],
+    };
   },
   methods: {
     checkAccount() {
       axios
         .post(`${url}/api/user/check`)
         .then((res) => {
-          console.log(res);
+          this.getProducts();
         })
         .catch((err) => {
           alert(err.data.message);
           window.location = "login.html";
+        });
+    },
+    getProducts() {
+      axios
+        .get(`${url}/api/${path}/admin/products`)
+        .then((res) => {
+          console.log("產品", res.data.products);
+          this.products = res.data.products;
+        })
+        .catch((err) => {
+          alert(err.data.message);
         });
     },
   },
